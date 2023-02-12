66f372c441866ef92172cb4dc4953d37a303aa0e
CodingFactoryL <leon.f662011gmail.com>
2023-02-12 16:27:59 +0100

    Kontaktformular eingefügt

--- kontakt/script.js
+++ kontakt/script.js
@@ -1,5 +1,21 @@
 function emailProgrammOeffnen(){
+    let geschlecht = document.getElementById("geschlechtFeld").value;
+    let name = document.getElementById("namenFeld").value;
+    let telefonnummer = document.getElementById("telefonNummernFeld").value;
     let betreff = document.getElementById("betreffFeld").value;
     let nachricht = document.getElementById("nachrichtenFeld").value;
-    window.open(`mailto:codingfactoryl0@gmail.com?subject=${betreff}&body=${nachricht}`);
+
+    if(geschlecht === "Divers"){
+        geschlecht = "";
+    }
+
+    nachricht += `\n\n\nvon ${geschlecht} ${name}`
+
+    if(telefonnummer !== ""){
+        nachricht += ` mit der Telefonnummer ${telefonnummer}`;
+    }
+
+    if(geschlecht !== "" &&  name !== "" && betreff !== "" && nachricht !== ""){
+        window.open(`mailto:codingfactoryl0@gmail.com?subject=${betreff}&body=${nachricht}`);
+    }
 }
