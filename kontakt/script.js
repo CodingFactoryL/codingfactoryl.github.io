b6c33cd7cc5c427837e7351018b9ca61bcf2a879
CodingFactoryL <125046877+CodingFactoryL@users.noreply.github.com>
2024-06-16 12:56:09 +0200

    new design contact

--- kontakt/script.js
+++ kontakt/script.js
@@ -1,27 +1,10 @@
-function emailProgrammOeffnen(){
-    let geschlecht = document.getElementById("geschlechtFeld").value;
-    let name = document.getElementById("namenFeld").value;
-    let telefonnummer = document.getElementById("telefonNummernFeld").value;
-    let betreff = document.getElementById("betreffFeld").value;
-    let nachricht = document.getElementById("nachrichtenFeld").value;
 
-    if(geschlecht === "Divers"){
-        geschlecht = "";
-    }
 
-    nachricht += `\n\r\n\r\n\rvon ${geschlecht} ${name}`
 
-    if(telefonnummer !== ""){
-        nachricht += ` mit der Telefonnummer ${telefonnummer}`;
-    }
-
-    if(geschlecht !== "" &&  name !== "" && betreff !== "" && nachricht !== ""){
-        window.open(`mailto:codingfactoryl12@gmail.com?subject=${betreff}&body=${nachricht}`);
-    }
+function emailContactButtonClicked(){
+    window.open('mailto:codingfactorylstudios@sfn-kassel.de');
 }
 
-function telefonnummerGeaendert(){
-    if(parseInt(document.getElementById("telefonNummernFeld").value, 10) < 0){
-        document.getElementById("telefonNummernFeld").value = "";
-    }
+function discordContactButtonClicked(){
+    window.open('https://discord.gg/deftTGQzb2');
 }
